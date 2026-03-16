6d0a841d1925a6993760b184ac9717a4cf0c1878
MuriloHallgren <murilohfonseca25@gmail.com>
2026-03-16 19:54:37 -0300

    outro teste

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ async def root():
 
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
